723c2c7675936c7796a9de7477bf8ad6aa9d5da9
Efe Tan <alonepirate02@gmail.com>
2026-03-29 00:04:05 +0300

    StaminaAdded

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
  
 [RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
@@ -9,20 +10,28 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Camera playerCamera;
     [SerializeField] private float walkSpeed = 6f;
     [SerializeField] private float runSpeed = 10f;
+    [SerializeField] private float stamina = 100f;
+    [SerializeField] private float staminaRecovery = 10f;
+    [SerializeField] private float staminaStunDuration = 3f;
     [SerializeField] private float jumpPower = 7f;
     [SerializeField] private float gravity = 10f;
 
 
     [SerializeField] private float lookSpeed = 2f;
     [SerializeField] private float lookXLimit = 70f;
+
+
+    public Image staminaBarRight;
+    public Image staminaBarLeft;
  
  
     Vector3 moveDirection = Vector3.zero;
     float rotationX = 0;
  
     public bool canMove = true;
+    public bool canRun = true;
+    public bool fillStamina = true;
     
- 
     
     CharacterController characterController;
     void Start()
@@ -42,20 +51,19 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-
+        // MAIN MOVEMENT CALCULATIONS
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
  
         bool isCrouching = Input.GetKey(KeyCode.LeftControl);
-        bool isRunning = Input.GetKey(KeyCode.LeftShift) && !isCrouching;
-
+        bool isRunning = Input.GetKey(KeyCode.LeftShift) && !isCrouching && canRun;
 
         float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
         float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
         float movementDirectionY = moveDirection.y;
         moveDirection = (forward * curSpeedX) + (right * curSpeedY);
  
-        if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
+        if (Input.GetButton("Jump") && canMove && characterController.isGrounded && !isCrouching)
         {
             moveDirection.y = jumpPower;
         }
@@ -64,8 +72,32 @@ public class PlayerMovement : MonoBehaviour
             moveDirection.y = movementDirectionY;
         }
 
+        //RUNNING & STAMINA
+        if (isRunning)
+        {
+            stamina -= Time.deltaTime * 30f;
+            if (stamina <= 0f)
+            {
+                stamina = 0f;
+                isRunning = false;
 
-        if(isCrouching)
+                StartCoroutine(DisableRunningUntilStamina());
+            }
+        }
+        else
+        {
+            if (fillStamina)
+            {
+                stamina += Time.deltaTime * staminaRecovery;
+                if (stamina > 100f) stamina = 100f;
+            }
+        }
+
+        staminaBarRight.fillAmount = stamina / 100f;
+        staminaBarLeft.fillAmount = stamina / 100f;
+
+        // CROUCHING & CEILING CHECK
+        if (isCrouching)
         {
             characterController.height = 1f;
         }
@@ -84,7 +116,8 @@ public class PlayerMovement : MonoBehaviour
                 characterController.height = 2f;
             }
         }
- 
+
+        // GRAVITY & MOVEMENT
         if (!characterController.isGrounded)
         {
             moveDirection.y -= gravity * Time.deltaTime;
@@ -99,5 +132,16 @@ public class PlayerMovement : MonoBehaviour
             playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
             transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
         }
+
+    }
+
+    IEnumerator DisableRunningUntilStamina()
+    {
+        canRun = false;
+        fillStamina = false;
+        yield return new WaitForSeconds(staminaStunDuration);
+        fillStamina = true;
+        yield return new WaitUntil(() => stamina >= 20f);
+        canRun = true;
     }
 }
